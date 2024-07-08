a06f4428bd97f0d01f175f5266b35fb803622254
Calnix <calnix.289@gmail.com>
2024-07-08 21:37:02 +0800

    ECDSA draft

--- src/ECDSADistributor.sol
+++ src/ECDSADistributor.sol
@@ -2,42 +2,32 @@
 
 pragma solidity 0.8.24;
 
-import "openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+import "openzeppelin-contracts/contracts/utils/cryptography/EIP712.sol";
 
 import {SafeERC20, IERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable2Step, Ownable} from "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
 import {Pausable} from "openzeppelin-contracts/contracts/utils/Pausable.sol";
-import {AccessControl} from "openzeppelin/contracts/access/AccessControl.sol";
+import {AccessControl} from "openzeppelin-contracts/contracts/access/AccessControl.sol";
 
 //import {ReentrancyGuard} from "@looksrare/contracts-libs/contracts/ReentrancyGuard.sol";
 
-contract ECDSADistributor is EIP712, Pausable, AccessControl {
+contract ECDSADistributor is EIP712, Pausable, AccessControl, Ownable2Step {
     using SafeERC20 for IERC20;
 
-    address public immutable TOKEN;
-
-    address public immutable STORED_SIGNER; //note: if change, should redeploy.
-
-    // Current round (users can only claim pending protocol fees for the current round)
-    uint256 public currentRound;
+    IERC20 internal immutable TOKEN;
+    address internal immutable STORED_SIGNER; //note: if change, should redeploy.
 
     // Users can claim until this timestamp
     uint256 public deadline;
-
-    // Max amount per user in current tree
-    uint256 public maximumAmountPerUserInCurrentRound;
+    address public operator;
 
     struct Claim {
         address user;
         uint128 round;
         uint128 amount;
     }
-
-    mapping(bytes32 signature => bool claimed) public hasClaimed;
     
-    uint256 public currentRound;
-
     struct RoundData {
         uint128 startTime;
         uint128 maximumAmountPerUser;
@@ -45,7 +35,8 @@ contract ECDSADistributor is EIP712, Pausable, AccessControl {
         uint128 claimedTokens;
     }
 
-    mapping(uint256 round => RoundData roundData) public rounds;
+    mapping(uint256 round => RoundData roundData) public allRounds;
+    mapping(bytes signature => bool claimed) public hasClaimed;
 
     // errors
     error AlreadyClaimed();
@@ -53,29 +44,28 @@ contract ECDSADistributor is EIP712, Pausable, AccessControl {
     error ClaimPeriodEnded();
     error AmountHigherThanMax();
     error RoundFullyClaimed();
-
     error InvalidSignature();
     error ECDSAZeroAddress();
 
     // events
     event Claimed(address indexed user, uint128 indexed round, uint128 amount);
     event ClaimedAll(address indexed user, uint128[] indexed rounds, uint128 totalAmount);
+    event DeadlineUpdated(uint256 newDeadline);
 
-    constructor(string memory _name, string memory _version, address admin) EIP712(_name, _version) {
+    constructor(string memory _name, string memory _version, address token, address storedSigner, address owner) EIP712(_name, _version) Ownable(owner) {
         
-        STORED_SIGNER = admin;
+        TOKEN = IERC20(token);
+        STORED_SIGNER = storedSigner;
     }   
 
     /*//////////////////////////////////////////////////////////////
                                  CLAIM
     //////////////////////////////////////////////////////////////*/
 
-    function claim(uint128 round, uint128 amount, bytes calldata signature) external {
+    function claim(uint128 round, uint128 amount, bytes calldata signature) external whenNotPaused {
         
         // check that signature has already been used: replay attack protection
-        if (hasClaimed[signature]) {
-            revert AlreadyClaimed();
-        }
+        if (hasClaimed[signature]) revert AlreadyClaimed();
 
         // check that deadline as not been exceeded; if deadline has been defined
         if(deadline > 0) {
@@ -84,22 +74,16 @@ contract ECDSADistributor is EIP712, Pausable, AccessControl {
             }
         }
         
-        RoundData memory roundData = rounds[round];
+        RoundData memory roundData = allRounds[round];
 
         // check that round has begun
-        if(roundData.startTime > block.timestamp){
-            revert RoundNotStarted();
-        }
+        if(roundData.startTime > block.timestamp) revert RoundNotStarted();
 
         // sanity check: max amt per user
-        if (amount > roundData.maximumAmountPerUser) {
-            revert AmountHigherThanMax();
-        }
+        if (amount > roundData.maximumAmountPerUser) revert AmountHigherThanMax();
 
         // sanity check: round not fully claimed
-        if (roundData.depositedTokens == roundData.claimedTokens) {
-            revert RoundFullyClaimed(); 
-        }
+        if (roundData.depositedTokens == roundData.claimedTokens) revert RoundFullyClaimed(); 
 
         // sig.verification
         _claim(round, amount, signature);
@@ -109,19 +93,14 @@ contract ECDSADistributor is EIP712, Pausable, AccessControl {
 
         // update storage
         hasClaimed[signature] = true;
-        rounds[round] = roundData;
+        allRounds[round] = roundData;
         
         emit Claimed(msg.sender, round, amount);
 
         TOKEN.safeTransfer(msg.sender, amount);
     }
 
-    function claimAll(uint128[] rounds, uint128[] amounts, bytes[] calldata signatures) external {
-
-        // check that signature has already been used: replay attack protection
-        if (hasClaimed[signature]) {
-            revert AlreadyClaimed();
-        }
+    function claimAll(uint128[] calldata rounds, uint128[] calldata amounts, bytes[] calldata signatures) external whenNotPaused {
 
         // check that deadline as not been exceeded; if deadline has been defined
         if(deadline > 0) {
@@ -129,34 +108,35 @@ contract ECDSADistributor is EIP712, Pausable, AccessControl {
                 revert ClaimPeriodEnded();
             }
         }
+        
+        uint256 roundsLength = rounds.length;
+        uint256 amountsLength = amounts.length;
+        uint256 signaturesLength = signatures.length;
 
-        uint256 arrLength = rounds.length;
-        //require() arr length check not 0. and equal.
+        require(roundsLength == amountsLength && roundsLength == signaturesLength, "Incorrect legnths");
+        require(roundsLength > 0, "Empty array");
 
-        uint256 totalAmount;
-        for(uint256 i = 0; i < arrLength; ++i) {
+        uint128 totalAmount;
+        for(uint256 i = 0; i < roundsLength; ++i) {
             
             // get round no. & round data
             uint128 round = rounds[i];
             uint128 amount = amounts[i];
             bytes memory signature = signatures[i];
 
-            RoundData memory roundData = rounds[round];
+            RoundData memory roundData = allRounds[round];
+
+            // check that signature has already been used: replay attack protection
+            if (hasClaimed[signature]) revert AlreadyClaimed();
 
             // check that round has begun
-            if(roundData.startTime > block.timestamp){
-                revert RoundNotStarted();
-            }
+            if(roundData.startTime > block.timestamp) revert RoundNotStarted();
 
             // sanity check: max amt per user
-            if (amount > roundData.maximumAmountPerUser) {
-                revert AmountHigherThanMax();
-            }
+            if (amount > roundData.maximumAmountPerUser) revert AmountHigherThanMax();
 
             // sanity check: round not fully claimed
-            if (roundData.depositedTokens == roundData.claimedTokens) {
-                revert RoundFullyClaimed(); 
-            }
+            if (roundData.depositedTokens == roundData.claimedTokens) revert RoundFullyClaimed(); 
 
             // sig.verification
             _claim(round, amount, signature);
@@ -165,13 +145,11 @@ contract ECDSADistributor is EIP712, Pausable, AccessControl {
             roundData.claimedTokens += amount;
             totalAmount += amount;       
 
-            // update storage: signature
+            // update storage: signature + roundData
             hasClaimed[signature] = true; 
+            allRounds[round] = roundData;
         }
         
-        // update storage: roundData
-        rounds[round] = roundData;
-
         emit ClaimedAll(msg.sender, rounds, totalAmount);
 
         TOKEN.safeTransfer(msg.sender, totalAmount);
@@ -186,12 +164,26 @@ contract ECDSADistributor is EIP712, Pausable, AccessControl {
             if(signer == address(0)) revert ECDSAZeroAddress(); // note: is this needed given the earlier
     }
 
-    function updateDeadline(uint256 deadline) external onlyOwner {
+    function updateDeadline(uint256 newDeadline) external onlyOwner {
+        require(block.timestamp < newDeadline, "Invalid deadline");
+
+        deadline = newDeadline;
+        emit DeadlineUpdated(newDeadline);
     }
 
     function updateRoundData() external onlyOwner {
         
     }
+
+    function deposit() external {
+        require(msg.sender == operator, "Incorrect caller");
+    }
+
+    function withdraw() external {
+        require(msg.sender == operator, "Incorrect caller");
+
+    }
+
 
     /*//////////////////////////////////////////////////////////////
                                 PAUSABLE
@@ -200,14 +192,14 @@ contract ECDSADistributor is EIP712, Pausable, AccessControl {
     /**
      * @notice Pause claim
      */
-    function pause() external onlyRole(OPERATOR_ROLE) whenNotPaused {
+    function pause() external onlyOwner whenNotPaused {
         _pause();
     }
 
     /**
      * @notice Unpause claim
      */
-    function unpause() external onlyRole(OPERATOR_ROLE) whenPaused {
+    function unpause() external onlyOwner whenPaused {
         _unpause();
     }
 
